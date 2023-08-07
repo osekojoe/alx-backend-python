304dabe7640c9f807477cff55f5d1c528e597a8f
jdev <20902932+osekojoe@users.noreply.github.com>
2023-08-07 17:24:17 +0200

    Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is
+being called.
+'''
+
+
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''execute multiple coroutines at the same time with async'''
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
